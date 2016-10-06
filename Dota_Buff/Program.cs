3e8a2dc99250969cb4448548e54106425c0e1db2
DarkP0wer <slipknot292@gmail.com>
2016-10-06 19:14:10 +0300

    Update Program.cs

--- Dota_Buff/Program.cs
+++ Dota_Buff/Program.cs
@@ -545,20 +545,21 @@ namespace DotaBuff_Overlay
             //DragLines
             if (IsUnderCaption())
             {
+                int w = 1;
                 Vector2 up_arrow = Game.MouseScreenPosition + new Vector2(0, -21);
-                DrawLine(Game.MouseScreenPosition, up_arrow, 2, new ColorBGRA(255, 255, 255, 255));
-                DrawLine(up_arrow, up_arrow + new Vector2(6, 6), 2, new ColorBGRA(255, 255, 255, 255));
-                DrawLine(up_arrow, up_arrow + new Vector2(-6, 6), 2, new ColorBGRA(255, 255, 255, 255));
+                DrawLine(Game.MouseScreenPosition, up_arrow, w, new ColorBGRA(255, 255, 255, 255));
+                DrawLine(up_arrow, up_arrow + new Vector2(6, 6), w, new ColorBGRA(255, 255, 255, 255));
+                DrawLine(up_arrow, up_arrow + new Vector2(-6, 6), w, new ColorBGRA(255, 255, 255, 255));
 
                 Vector2 right_arrow = Game.MouseScreenPosition + new Vector2(21, 0);
                 DrawLine(Game.MouseScreenPosition, right_arrow, 2, Color.White);
-                DrawLine(right_arrow, right_arrow + new Vector2(-6, -6),2, Color.White);
-                DrawLine(right_arrow, right_arrow + new Vector2(-6, 6),2, Color.White);
+                DrawLine(right_arrow, right_arrow + new Vector2(-6, -6), w, Color.White);
+                DrawLine(right_arrow, right_arrow + new Vector2(-6, 6), w, Color.White);
 
                 Vector2 left_arrow = Game.MouseScreenPosition + new Vector2(-21, 0);
-                DrawLine(Game.MouseScreenPosition, left_arrow, 2,Color.White);
-                DrawLine(left_arrow, left_arrow + new Vector2(6, -6), 2, Color.White);
-                DrawLine(left_arrow, left_arrow + new Vector2(6, 6), 2, Color.White);
+                DrawLine(Game.MouseScreenPosition, left_arrow, w,Color.White);
+                DrawLine(left_arrow, left_arrow + new Vector2(6, -6), w, Color.White);
+                DrawLine(left_arrow, left_arrow + new Vector2(6, 6), w, Color.White);
             }
         }
 
